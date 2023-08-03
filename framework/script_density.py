0954efcdea0be5b4a880a2bf27b577d51270ffd3
Andrea Pinto <andreakiro.pinto@gmail.com>
2023-08-03 17:42:39 +0200

    integrate kme encoder in density script

--- framework/script_density.py
+++ framework/script_density.py
@@ -1,20 +1,33 @@
 from util import visualizer
 from manifold import manifold
-# from density import kme_density
+from torchkme import KMeansEncoder
 import argparse
+import torch
 import time
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 
-SAMLPES_PER_RENDER = 100
+# VISUALIZER
+SAMLPES_PER_RENDER = 1
 MAX_SAMPLES_EXPERIMENT = 1e9
 MIN_TIME_RENDER = 0.01
 INTERFACE_SCALE = 0.25
 RW_STEP_SIZE = 0.2
 
+# PARSER
 MANIFOLDS = ['euclidean', 'spherical', 'toroidal', 'hyperpara', 'hyperboloid']
 SAMPLERS = ['uniform', 'gaussian', 'vonmises_fisher']
 INTERFACES = ['constant', 'xtouch']
 SAMPLING = ['rw', 'sample']
+
+# KME
+K = 100
+LR = 0.01
+BALANCING_STRENGHT = 0.1
+HOMEOSTASIS = True
+INIT_METHOD = 'zeros'
 
 
 def get_args() -> argparse.Namespace:
@@ -72,9 +85,12 @@ def renderloop() -> None:
         elif args.sampling_type == 'rw':
             points = m.random_walk(SAMLPES_PER_RENDER, points[-1] if points is not None else None, RW_STEP_SIZE)
         
+        kme_enc.update(torch.tensor(points))
         num_samples += SAMLPES_PER_RENDER
-        data = {'name': 'samples', 'points': points, 'color': [0, 255, 0]}
-        visualizer.add(data)
+        state_points = {'name': 'samples', 'points': points, 'color': [0, 255, 0]}
+        centroids = {'name': 'centroids', 'points': kme_enc.centroids, 'color': [255, 0, 0]}
+        visualizer.add(state_points)
+        visualizer.add(centroids)
         visualizer.render()
         time_end = time.time()
         time_elapsed = time_end - time_start
@@ -86,4 +102,5 @@ if __name__ == '__main__':
     args = get_args()
     m = get_manifold(args)
     visualizer = visualizer.Visualizer(interface=args.interface, defaults={'scale': INTERFACE_SCALE})
+    kme_enc = KMeansEncoder(K, m.ambient_dim, LR, BALANCING_STRENGHT, HOMEOSTASIS, INIT_METHOD)
     renderloop()
