7c5dbc0813dcd23927fcc77817bc6ac9377f4608
Andrea Pinto <andreakiro.pinto@gmail.com>
2023-08-03 15:41:26 +0200

    open kmeans density encoder visibility

--- kme.all/kme.py/torchkme/__init__.py
+++ kme.all/kme.py/torchkme/__init__.py
@@ -1,4 +1,5 @@
 from .rewarder import KMERewarder
 from .sampling import sample_from
+from .kmeans import KMeansEncoder
 
-__all__ = ["KMERewarder", "sample_from"]
+__all__ = ["KMERewarder", "sample_from", "KMeansEncoder"]
