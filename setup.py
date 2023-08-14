dafde81bd80179e2c8bfe8cf0dc80d4c74c8d163
Andrea Pinto <andreakiro.pinto@gmail.com>
2023-08-14 23:53:46 +0200

    add requirements to rum setup

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
+def requirements():
+    with open('requirements.txt', 'r') as reqs:
+        return [line.strip() for line in reqs if not line.startswith('#')]
+
 setup(
   name = 'rum',
   packages=['rum'],
-  package_data={}
+  package_data={},
+  install_requires=requirements()
 )
