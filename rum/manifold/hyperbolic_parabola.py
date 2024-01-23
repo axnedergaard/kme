89ffd1a730670f21bfebbca73acc22265f6dff6a
Alexander Nedergaard <axnedergaard@gmail.com>
2024-01-23 15:13:17 +0100

    Add proper hyperbolic parabola

--- rum/manifold/hyperbolic_parabola.py
+++ rum/manifold/hyperbolic_parabola.py
@@ -1,11 +1,27 @@
+import numpy as np
+import itertools
 from .manifold import Manifold
 
 class HyperbolicParabolaManifold(Manifold):
-  def __init__(self, dim):
+  def __init__(self, dim, sampler):
+    del sampler # TODO.
     assert dim == 2 # TODO.
     super(HyperbolicParabolaManifold, self).__init__(dim, dim + 1)
     self.low = -1.0 
     self.high = 1.0 
+
+  def retraction(self, p, v):
+    if np.any(v != 0.0):
+      norm_v = self._metric_size(p, v / np.linalg.norm(v))
+      v /= norm_v
+    xi = self._to_local(p)
+    xi[0] += v[0]
+    xi[1] += v[1] 
+    xi = np.clip(xi, self.low, self.high)
+    return self._from_local(xi)
+
+  def starting_state(self):
+    return self._from_local([0.0, 0.0])
 
   def pdf(self, x):
     raise NotImplementedError
@@ -16,25 +32,31 @@ class HyperbolicParabolaManifold(Manifold):
     u, v = np.random.uniform(self.low, self.high, 2)
     return self._from_local([u, v])
 
-  def starting_state(self):
-    return np.zeros(self.ambient_dim)
+  def metric_tensor(self, p):
+    return np.array([
+      [1.0 + 4.0 * p[0] ** 2, - 4.0 * p[0] * p[1]],
+      [- 4.0 * p[0] * p[1], 1.0 + 4.0 * p[1] ** 2]
+    ])
 
-  def distance_function(self, x, y):
-    raise NotImplementedError
+  def impicit_function(self, p):
+    return p[0]**2 - p[1]**2
 
-  def metric_tensor(self, x):
-    raise NotImplementedError
+  def grid(self, n):
+    m = int(np.sqrt(n))
+    points = []
+    linspace = np.linspace(self.low, self.high, m)
+    for i, j in itertools.product(linspace, repeat=2):
+      points.append(self._from_local([i, j]))
+    points = np.array(points)
+    return points
 
-  def impicit_function(self, c):
-    return c[0]**2 - c[1]**2
-
-  def _to_local(self, c):
-    u = c[0]
-    v = c[1]
+  def _to_local(self, p):
+    u = p[0]
+    v = p[1]
     return np.array([u, v])
 
-  def _from_local(self, c):
-    x = c[0]
-    y = c[1]
-    z = c[0] * c[1]
+  def _from_local(self, xi):
+    x = xi[0]
+    y = xi[1]
+    z = xi[0] ** 2 - xi[1] ** 2
     return np.array([x, y, z])
