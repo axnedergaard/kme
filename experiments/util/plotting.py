b3987c3337b735e505a005d8b59493051c6bd4ce
Alexander Nedergaard <axnedergaard@gmail.com>
2024-02-09 22:35:10 +0100

    Add entropy ranking plotting script

--- experiments/util/plotting.py
+++ experiments/util/plotting.py
@@ -319,3 +319,46 @@ def scale_independent_loss_distance_vs_steps(data, **kwargs):
         ax.legend(loc="upper left")
     
     return fig
+
+def entropy_ranking(data, **kwargs):
+  def get_label(config):
+    # Return sampler label.
+    # For now, samplers are always on manifolds.
+    manifold_name = config['manifold']['name']
+    sampler_name = config['manifold']['sampler']['type'].capitalize()
+    return f"{manifold_name}-{sampler_name}"
+
+  def extract_and_format_data(data):
+    entropies = {}
+    labels = []
+    # Add each entropy and sampler label to list at density estimatior key.
+    for exp_data in data:
+      label = get_label(exp_data.config)
+      # If there is an entropy but no density, the manifold (ground truth) was used to compute entropy.
+      density_name = exp_data.config['density']['name'] if exp_data.config['density'] != {} else "True" 
+      entropies.setdefault(density_name, []).append([exp_data["entropy"], label])
+      if label not in labels:
+        labels.append(label)
+    # Sort each list according to sampler labels. # TODO. Somehow need to have ordering based on actual entropies, not string ordering.
+    for key in entropies:
+      entropies[key].sort(key=lambda x: x[1])
+      for i in range(len(entropies[key])):
+        # Remove sampler label tags used for sorting.
+        values = entropies[key][i][0]
+        # Average entropies where density estimator and labels are the same.
+        entropies[key][i] = np.mean(values)
+    return entropies, labels
+
+  fig, ax = plt.subplots()
+  entropies, labels = extract_and_format_data(data)
+
+  for i, (key, value) in enumerate(entropies.items()):
+    assert len(value) == len(labels)
+    ax.bar(range(len(value)), value, label=key, color=COLORS[i])
+
+  ax.set_xticks(range(len(labels)), labels)
+  ax.set_xlabel("Density")
+  ax.set_ylabel("Entropy")
+  ax.set_title("Entropy ranking")
+
+  return fig
