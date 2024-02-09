2f4b329807dc5bf3a6151d78eb4c9ff47849c0cd
Andrea Pinto <andreakiro.pinto@gmail.com>
2024-02-09 17:06:02 -0500

    add pdf and distance plotting script

--- experiments/util/plotting.py
+++ experiments/util/plotting.py
@@ -227,138 +227,103 @@ def count_variance_vs_beta(data, **kwargs):
     return fig
 
 
-def scale_independent_loss_pdf_vs_steps(data, **kwargs):
+def pdf_loss_vs_steps(data, **kwargs):
     """
     Plot the scale independant loss pdf vs steps with confidence interval
     :params: data (undefined) : The data to plot
     """
 
-    def extract_and_format_data(data, **kwargs):
-        
-        experiments = []
+    def extract_and_format_data(data):
+
+        groups = {} # Each group is a different density
 
         for exp_data in data:
-            # Grab the steps range for that experiment
-            steps = exp_data["_step"]
-            # Grab the scale independant loss pdf for that experiment
-            scale_independant_loss_pdf = exp_data["scale_independent_loss"]
-            experiments.append((steps, scale_independant_loss_pdf))
+            group_key = exp_data.config['density']['name']
+            # Grab the pdf loss for that experiment
+            pdf_loss = exp_data["pdf_loss"]
+            # Make sure the group exists
+            if group_key not in groups:
+                groups[group_key] = {}
+                groups[group_key]["pdf_loss"] = []
+            # Append the pdf loss to the group
+            # Those values will get averaged and plotted
+            groups[group_key]["pdf_loss"].append(pdf_loss)
 
-        # Assert all the sublists have the same length
-        unique_lengths = {len(l) for exp in experiments for l in exp}
-        assert len(unique_lengths) == 1 and all(len(tup[0]) == len(tup[1]) for tup in experiments)
+        for group_key, group in groups.items():
+            # Assert all the sublists have the same length
+            assert len(set([len(l) for l in group['pdf_loss']])) == 1, "All pdf_loss sublists must have the same length"
 
-        # Grab the data we need
-        steps = experiments[0][0]
-        scale_independant_loss_pdf = np.array([tup[1] for tup in experiments])
-        label = "Scale independent loss pdf"
+        for idx, (group_key, _) in enumerate(groups.items()):
+            # Assign a color to each group for the plot
+            groups[group_key]["color"] = COLORS[idx]
 
-        return (steps, scale_independant_loss_pdf, COLORS[0], label)
+        # Grab the timesteps range for the plot (same for all groups)
+        timesteps = np.array(list(range(len(groups[list(groups.keys())[0]]['pdf_loss'][0]))))
+
+        return [
+            (timesteps, np.array(group['pdf_loss']), group['color'], group_key)
+            for group_key, group in groups.items()
+        ]
 
     fig, ax = plt.subplots()
-    steps, scale_independant_loss_pdf, color, label = extract_and_format_data(data, **kwargs)
-    plot_with_confidence_interval(ax, steps, scale_independant_loss_pdf, color, label)
+    for timesteps, pdf_loss, color, label in extract_and_format_data(data):
+        plot_with_confidence_interval(ax, timesteps, pdf_loss, color, label)
 
-    ax.set_xlabel("Steps")
-    ax.set_ylabel("Scale Independant Loss PDF")
-    ax.set_title("Scale Independant Loss PDF vs Steps")
+    ax.set_xlabel("Timesteps")
+    ax.set_ylabel("Probability Density Function Loss")
+    ax.set_title(f"Probability Density Function Loss")
 
-    if kwargs.get("grid", True):
-        # by default the grid is shown
-        ax.grid(visible=True)
-
-    if kwargs.get("legend", True):
-        # by default the legend is shown
-        ax.legend(loc="upper left")
+    # TODO. Add grid and legend stuff.
     
     return fig
 
 
-def scale_independent_loss_distance_vs_steps(data, **kwargs):
+def distance_loss_vs_steps(data, **kwargs):
     """
-    Plot the scale independant loss distance vs steps with confidence interval
+    Plot the scale independant loss pdf vs steps with confidence interval
     :params: data (undefined) : The data to plot
     """
 
-    def extract_and_format_data(data, **kwargs):
-                
-        experiments = []
+    def extract_and_format_data(data):
+
+        groups = {} # Each group is a different density
 
         for exp_data in data:
-            # Grab the steps range for that experiment
-            steps = exp_data["_step"]
-            # Grab the scale independant distance_loss for that experiment
-            scale_independant_loss_pdf = exp_data["distance_loss"]
-            experiments.append((steps, scale_independant_loss_pdf))
+            group_key = exp_data.config['geometry']['name']
+            # Grab the pdf loss for that experiment
+            distance_loss = exp_data["distance_loss"]
+            # Make sure the group exists
+            if group_key not in groups:
+                groups[group_key] = {}
+                groups[group_key]["distance_loss"] = []
+            # Append the pdf loss to the group
+            # Those values will get averaged and plotted
+            groups[group_key]["distance_loss"].append(distance_loss)
 
-        # Assert all the sublists have the same length
-        unique_lengths = {len(l) for exp in experiments for l in exp}
-        assert len(unique_lengths) == 1 and all(len(tup[0]) == len(tup[1]) for tup in experiments)
+        for group_key, group in groups.items():
+            # Assert all the sublists have the same length
+            assert len(set([len(l) for l in group['distance_loss']])) == 1, "All distance_loss sublists must have the same length"
 
-        # Grab the data we need
-        steps = experiments[0][0]
-        scale_independant_loss_pdf = np.array([tup[1] for tup in experiments])
-        label = "Scale independent distance loss"
+        for idx, (group_key, _) in enumerate(groups.items()):
+            # Assign a color to each group for the plot
+            groups[group_key]["color"] = COLORS[idx]
 
-        return (steps, scale_independant_loss_pdf, COLORS[0], label)
+        # Grab the timesteps range for the plot (same for all groups)
+        timesteps = np.array(list(range(len(groups[list(groups.keys())[0]]['distance_loss'][0]))))
+
+        return [
+            (timesteps, np.array(group['distance_loss']), group['color'], group_key)
+            for group_key, group in groups.items()
+        ]
 
     fig, ax = plt.subplots()
-    steps, scale_independant_loss_distance, color, label = extract_and_format_data(data, **kwargs)
-    plot_with_confidence_interval(ax, steps, scale_independant_loss_distance, color, label)
+    for timesteps, distance_loss, color, label in extract_and_format_data(data):
+        plot_with_confidence_interval(ax, timesteps, distance_loss, color, label)
 
-    ax.set_xlabel("Steps")
-    ax.set_ylabel("Scale Independant Loss Distance")
-    ax.set_title("Scale Independant Loss Distance vs Steps")
+    ax.set_xlabel("Timesteps")
+    ax.set_ylabel("Distance Loss")
+    ax.set_title(f"Distance Loss")
 
-    if kwargs.get("grid", True):
-        # by default the grid is shown
-        ax.grid(visible=True)
-
-    if kwargs.get("legend", True):
-        # by default the legend is shown
-        ax.legend(loc="upper left")
+    # TODO. Add grid and legend stuff.
     
     return fig
-
-def entropy_ranking(data, **kwargs):
-  def get_label(config):
-    # Return sampler label.
-    # For now, samplers are always on manifolds.
-    manifold_name = config['manifold']['name']
-    sampler_name = config['manifold']['sampler']['type'].capitalize()
-    return f"{manifold_name}-{sampler_name}"
-
-  def extract_and_format_data(data):
-    entropies = {}
-    labels = []
-    # Add each entropy and sampler label to list at density estimatior key.
-    for exp_data in data:
-      label = get_label(exp_data.config)
-      # If there is an entropy but no density, the manifold (ground truth) was used to compute entropy.
-      density_name = exp_data.config['density']['name'] if exp_data.config['density'] != {} else "True" 
-      entropies.setdefault(density_name, []).append([exp_data["entropy"], label])
-      if label not in labels:
-        labels.append(label)
-    # Sort each list according to sampler labels. # TODO. Somehow need to have ordering based on actual entropies, not string ordering.
-    for key in entropies:
-      entropies[key].sort(key=lambda x: x[1])
-      for i in range(len(entropies[key])):
-        # Remove sampler label tags used for sorting.
-        values = entropies[key][i][0]
-        # Average entropies where density estimator and labels are the same.
-        entropies[key][i] = np.mean(values)
-    return entropies, labels
-
-  fig, ax = plt.subplots()
-  entropies, labels = extract_and_format_data(data)
-
-  for i, (key, value) in enumerate(entropies.items()):
-    assert len(value) == len(labels)
-    ax.bar(range(len(value)), value, label=key, color=COLORS[i])
-
-  ax.set_xticks(range(len(labels)), labels)
-  ax.set_xlabel("Density")
-  ax.set_ylabel("Entropy")
-  ax.set_title("Entropy ranking")
-
-  return fig
